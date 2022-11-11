6d502cdc8dc29ed7e57a707e2bdc244d8da0c08b
balancewins <balancewins@bk.ru>
2022-11-11 19:05:37 +0300

    start rework kanban to react

---
+++ src/components/app/app.js
@@ -0,0 +1,16 @@
+import Header from '../header/header';
+import Main from '../main/main';
+import '../../css/style.css';
+
+function App() {
+  return (
+    <div className="board-app">
+      <Header />
+      <main className="board-app__main">
+        
+      </main>
+    </div>
+  );
+}
+
+export default App;
